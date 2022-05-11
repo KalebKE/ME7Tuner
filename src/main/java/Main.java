7025f62dd0e21eed833251b9c9f9a060bd2773d3
KalebKE <kaleb.kircher@gmail.com>
2022-05-10 19:15:33 -0600

    issue_19
    * Set default locale to English

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,12 @@ import com.formdev.flatlaf.intellijthemes.FlatArcDarkOrangeIJTheme;
 import presentation.view.MainManager;
 
 import javax.swing.*;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.ENGLISH);
 
         try {
             UIManager.setLookAndFeel( new FlatArcDarkOrangeIJTheme());
